11dadb99f923a9e3d785cadb4a7155bcebcbad87
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2021-07-12 19:47:25 +0900

    Create filterChips.dart

---
+++ 0712/filterChips.dart
@@ -0,0 +1,44 @@
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    title: 'Filter Chips Demo',
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Filter Chips Demo'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            FilterChip(
+              label: Text('Filter 1'),
+              selected: true,
+              onSelected: (bool value) {},
+            ),
+            FilterChip(
+              label: Text('Filter 2'),
+              onSelected: (bool value) {},
+            ),
+            FilterChip(
+              label: Text('Filter 3'),
+              selected: true,
+              onSelected: (bool value) {},
+            ),
+            FilterChip(
+              label: Text('Filter 4'),
+              onSelected: (bool value) {},
+            ),
+            FilterChip(
+              label: Text('Filter 5'),
+              onSelected: (bool value) {},
+            ),
+            FilterChip(
+              label: Text('Filter 6'),
+              onSelected: (bool value) {},
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
