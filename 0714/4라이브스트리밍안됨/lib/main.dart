e4ac6558da9d8fd1f849420226a09f988cd09879
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2021-07-14 20:28:10 +0900

    Add files via upload

---
+++ 0714/4라이브스트리밍안됨/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:agora_0714_4/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
