ce8ace8ffdb7e59d9c5dc7039817f7f5c9780870
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2021-08-18 18:00:57 +0900

    Add files via upload

---
+++ 0818_BottomNav_Circle/lib/second.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spin App')
+      ),
+      body: Container(),
+    );
+  }
+}
